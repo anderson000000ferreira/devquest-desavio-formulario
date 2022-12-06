e4257e3ede29a2b5ffec820455e63c8c4e2fa2b4
Anderson Ferreira <andersonciamicro@hotmail.com>
2022-12-06 03:12:53 -0300

    Adicionado projeto formulario

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('form')
+const campos = document.querySelectorAll('.required')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    checkinput();
+})
+
+function checkinput() {  
+
+    campos.forEach((input, index,) => {
+        const spans = document.querySelectorAll('.infor-obrigatorio')
+    
+        if (input.value !== '') {
+            input.style.border = '2px solid #3CCC87';   
+            spans[index].style.display = 'none';     
+        
+        } else {
+            input.style.border = '2px solid #F52E2E';
+            spans[index].style.display = 'block';
+        }
+        })
+}
