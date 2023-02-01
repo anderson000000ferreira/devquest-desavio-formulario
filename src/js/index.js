32046aef64d99f3a8e217c525180584557e646c6
Anderson Ferreira <andersonciamicro@hotmail.com>
2023-02-01 19:17:04 -0300

    Adicionando o input com class

--- src/js/index.js
+++ src/js/index.js
@@ -13,12 +13,29 @@ function checkinput() {
         const spans = document.querySelectorAll('.infor-obrigatorio')
     
         if (input.value !== '') {
-            input.style.border = '2px solid #3CCC87';   
+            input.classList.add('aprovado'); 
             spans[index].style.display = 'none';     
         
         } else {
-            input.style.border = '2px solid #F52E2E';
+            input.classList.add('obrigatorio')
             spans[index].style.display = 'block';
         }
         })
 }
+
+
+// function checkinput() {  
+
+//     campos.forEach((input, index,) => {
+//         const spans = document.querySelectorAll('.infor-obrigatorio')
+    
+//         if (input.value !== '') {
+//             input.style.border = '2px solid #3CCC87';   
+//             spans[index].style.display = 'none';     
+        
+//         } else {
+//             input.style.border = '2px solid #F52E2E';
+//             spans[index].style.display = 'block';
+//         }
+//         })
+// }
